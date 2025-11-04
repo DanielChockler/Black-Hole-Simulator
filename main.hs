2ae01e852426714b1cf533c56b0a6cce0061c075
Daniel Chockler <daniel.chockler@gmail.com>
2025-11-04 13:16:46 +0000

    Fixed trail implementation, added polar coordinates

--- main.hs
+++ main.hs
@@ -20,13 +20,15 @@ type Trail = [Position]
 type Mass = Float
 
 data BlackHoleData = BlackHoleData {
-    bh_position :: Position,
+    bh_position_cartesian :: Position,
+    bh_position_polar :: Position,
     mass :: Mass,
     radius :: Float
 }
 
 data RayData = RayData {
-    ray_position :: Position,
+    ray_position_cartesian :: Position,
+    ray_position_polar :: Position,
     direction :: Direction,
     trail :: [Position]
 }
@@ -37,28 +39,34 @@ type Model = [Object]
 
 createBlackHole :: Position -> Mass -> BlackHoleData
 createBlackHole pos m = BlackHoleData {
-    bh_position = pos,
+    bh_position_cartesian = pos,
+    bh_position_polar = (0, 0),
     mass = m,
     radius = (2 * g * m) / (c ** 2)
 }
 
 createRay :: Position -> Direction -> Trail -> RayData
 createRay pos d t = RayData {
-    ray_position = pos,
+    ray_position_cartesian = pos,
+    ray_position_polar = ((fst pos) * (fst pos) + (snd pos) * (snd pos), atan ((snd pos) / (fst pos))),
     direction = d,
     trail = t
 }
 
 draw :: Model -> Picture
-draw []                     = Blank
+draw []     = Blank
 draw (x:xs) = pictures (drawObject x : [draw xs])
     where
-    drawObject (BlackHole bh) = translate (fst (bh_position bh)) (snd (bh_position bh)) $ color red $ circleSolid (radius bh)
-    drawObject (Ray ray)      = pictures [
-                                translate (fst (ray_position ray)) (snd (ray_position ray)) $ color white $ circleSolid 1,
-                                color (makeColor 1.0 1.0 1.0 fromIntegral((length t / (length t + 1)))) $ lineLoop t]
-                                where
-                                t = trail ray
+    drawObject (BlackHole bh) = translate (fst (bh_position_cartesian bh)) (snd (bh_position_cartesian bh)) $ color red $ circleSolid (radius bh)
+    drawObject (Ray ray)      = pictures ([translate (fst (ray_position_cartesian ray)) (snd (ray_position_cartesian ray)) $ color white $ circleSolid 1] ++ drawTrail (reverse (take 100 (reverse (trail ray)))))
+        where
+        drawTrail :: Trail -> [Picture]
+        drawTrail positions | length positions < 2 = []
+                            | otherwise            = zipWith3 (drawSegment (length positions)) (init positions) (tail positions) [0..]
+
+        drawSegment totalLength pos1 pos2 i =
+            let alpha = fromIntegral i / fromIntegral totalLength
+            in color (makeColor 1.0 1.0 1.0 alpha) $ line [pos1, pos2]
 
 update vp dt model = map updateObject model
     where
@@ -68,17 +76,17 @@ update vp dt model = map updateObject model
 updateRay :: Object -> Float -> Model -> Object
 updateRay (Ray rd) dt model = Ray (createRay newPos (direction rd) updatedTrail)
     where
-    currentPos = ray_position rd
+    currentPos = ray_position_cartesian rd
     currentDir = direction rd
     newPos = (fst currentPos + fst currentDir * c * dt * coefficient,
               snd currentPos + snd currentDir * c * dt * coefficient)
     updatedTrail = (trail rd) ++ [currentPos]
 
 lightRow :: Int -> Model
-lightRow n = [Ray (createRay (-200, 25 * fromIntegral x) (1, 0) []) | x <- [0..n]]
+lightRow n = [Ray (createRay (-200, -100 + 25 * fromIntegral x) (1, 0) []) | x <- [0..n]]
 
 initial :: Model
-initial = BlackHole (createBlackHole (100, 100) 5.39e28) : lightRow 9
+initial = BlackHole (createBlackHole (0, 0) 5.39e28) : lightRow 9
 
 main :: IO ()
 main = simulate (InWindow "Window" (1500, 1500) (0, 0)) black 30 initial draw update
